af7de9e5a699cd5b891c81097503b710101b7353
git <william.duhe@hcahealthcare.com>
2023-10-05 07:45:28 -0500

    Added pip install command

--- build-files/custom.sh
+++ build-files/custom.sh
@@ -14,6 +14,6 @@ fi
 #                                                                                     #
 
 # Install packages
-pip install -r ../sr/requirements.txt
+pip install -r ../src/requirements.txt
 
 # Do NOT put an exit as this file is sourced.
