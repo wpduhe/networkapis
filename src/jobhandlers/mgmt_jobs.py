69ced3bc0b49ef470d972117df030e6c437c3220
git <william.duhe@hcahealthcare.com>
2024-06-26 13:49:21 -0500

    Added management status check and lots of logging

--- src/jobhandlers/mgmt_jobs.py
+++ src/jobhandlers/mgmt_jobs.py
@@ -3,6 +3,8 @@ from ipam.utils import ManagementJob
 from apic.classes import FabricNode
 from apic.utils import APIC
 from ipam.utils import NetworkAPIIPAM
+from types import SimpleNamespace
+import requests
 import logging
 import socket
 import json
@@ -25,6 +27,11 @@ logging.getLogger('urllib3').setLevel(logging.WARNING)
 logging.getLogger('github').setLevel(logging.WARNING)
 logging.getLogger('netmiko').setLevel(logging.WARNING)
 logging.getLogger('paramiko').setLevel(logging.WARNING)
+
+
+def jsonload(response: requests.Response) -> SimpleNamespace:
+    if response.ok:
+        return json.loads(response.text, object_hook=lambda x: SimpleNamespace(**x))
 
 
 def main():
@@ -51,6 +58,12 @@ def main():
         except (socket.herror, IndexError):
             return False
 
+    def complete(i_job: ManagementJob):
+        i_job.completion = f'{time.asctime()}: Request submitted by automation'
+        gh.add_file(file_path=f'{i_job.completed_jobs}/{file}', message='Completed ManagementJob',
+                    content=json.dumps(i_job.__dict__, indent=4))
+        gh.delete_file(file_path=f'{i_job.queue_path}/{file}', message='Completed ManagementJob')
+
     # Iterate over files in queue path
     for file in gh.list_dir(ManagementJob.queue_path):
         content = gh.get_file_content(file_path=f'{ManagementJob.queue_path}/{file}')
@@ -63,6 +76,8 @@ def main():
 
         env, node = re.search(r'([^_]+)_(\d+)', file).groups()
 
+        logger.debug(f'Attempting management of node {node} in {env}')
+
         # env = file.split('_')[0]
         # node = file.split('_')[1]
 
@@ -72,29 +87,40 @@ def main():
 
         parsed = re.split('[-.]', job.dns_template)
 
+        # Check to see if address is already managed
+        logger.debug(f'Checking management status of {job.ip}...')
+        addr = jsonload(ipam.get_address(job.ip))
+        if addr.userDefinedFields.Tools:
+            logger.debug(f'{job.ip} is already managed')
+            complete(job)
+
         # Check to see if a host record matching the DNS template has already been created for the given IP:
         #    Could have been by user or prior DNS creation delay caused checks to fail
         if check_record(parsed):
+            logger.debug(f'Management record already exists for {job.ip}')
             continue
 
         # Verify that the node firmware has been upgraded to match the APIC version, do not manage if not.
         if apic.version not in node.attributes.version:
+            logger.debug(f'Skipping management of {job.ip} because it is not yet code-leveled...')
             continue
 
         # Validate SSH access to the requested IP address
         if APIC.check_ssh_to_oob(job.ip):
+            logger.debug(f'SSH access to {job.ip} is present. Attempting to manage...')
             # with BIG() as big:
             #     status, _ = big.manage_device(dns_template=job.dns_template, ip=job.ip, ip_name='Not Provided')
-            ipam = NetworkAPIIPAM()
 
             r = ipam.manage_device(address=job.ip, dns_template=job.dns_template)
 
             if r.ok:
-                job.completion = f'{time.asctime()}: Request submitted by automation'
-                gh.add_file(file_path=f'{job.completed_jobs}/{file}', message='Completed ManagementJob',
-                            content=json.dumps(job.__dict__, indent=4))
-                gh.delete_file(file_path=f'{job.queue_path}/{file}', message='Completed ManagementJob')
+                logger.debug(f'Management successful for {job.ip}')
+                complete(job)
             else:
+                logger.debug(f'Management failed for {job.ip}')
+                logger.debug(f'Response Status Code: {r.status_code}')
+                logger.debug(f'Response Reason: {r.reason}')
+                logger.debug(f'Response payload: {json.dumps(r.json())}')
                 continue
 
     logger.debug(f'Management jobs completed by {socket.gethostname()}')
@@ -102,4 +128,5 @@ def main():
 
 if __name__ == '__main__':
     gh = GithubAPI()
+    ipam = NetworkAPIIPAM()
     main()
