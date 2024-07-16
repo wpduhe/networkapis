d47866e5b68b4061085f1764dcd0206a157a9057
git <william.duhe@hcahealthcare.com>
2024-07-16 08:08:54 -0500

    Updated VPC and NXOS.vpc

--- src/nexus/utils.py
+++ src/nexus/utils.py
@@ -409,7 +409,7 @@ class NXOS:
         if self.vpc is not None:
             return {
                 'VPC Domain': self.vpc.domain,
-                'Self': self.vpc.host,
+                'Self': self.host,
                 'Peer': self.vpc.peer,
                 'Hostname': self.hostname
             }
