ef8a88e689e9bd3807f3b897ae30d3072ae1fa6e
git <william.duhe@hcahealthcare.com>
2024-08-08 14:48:39 -0500

    Updating entrypoint.sh to account for working directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-exec python -m uvicorn src.main:app --host 0.0.0.0 --port ${PORT} --log-config loggingconfig.json
+exec python -m uvicorn main:app --host 0.0.0.0 --port ${PORT} --log-config loggingconfig.json
 wait -n
 exit $?
